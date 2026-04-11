a680ff4e445a4d6a2470ed395c7941895d1bf93f
IbraTagirovich <www.arteswin86@gmail.com>
2026-04-11 10:12:17 +0300

    task 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "4815162342";
+
+    public static Connection getConnection () {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("ОШИБКА ПОДКЛЮЧЕНИЯ К БД" + e.getMessage());
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void close (Connection connection) throws SQLException {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new SQLException(e);
+            }
+        }
+    }
 }
